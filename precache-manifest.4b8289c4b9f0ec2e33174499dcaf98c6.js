0c16ebfc201f11805ff757a651958083a0a1ff30
kevinjyee <kevinyee@utexas.edu>
2020-04-11 20:44:39 -0400

    Updates

---
+++ precache-manifest.4b8289c4b9f0ec2e33174499dcaf98c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b29324c45ced02ca418e097ae8f52c77",
+    "url": "/abs-with-kevin-2/static/media/antlogo.b29324c4.svg"
+  },
+  {
+    "revision": "7748d4841ba8b825dc0b5329be7c1b1b",
+    "url": "/abs-with-kevin-2/static/media/abswithkevin.7748d484.svg"
+  },
+  {
+    "revision": "e14f1cc1b930bfc5de44",
+    "url": "/abs-with-kevin-2/static/js/runtime~main.6540e16d.js"
+  },
+  {
+    "revision": "0fd6d5f6673700a5375f",
+    "url": "/abs-with-kevin-2/static/js/main.091f735c.chunk.js"
+  },
+  {
+    "revision": "ec49592d6712cb6b5e06",
+    "url": "/abs-with-kevin-2/static/js/2.5d6fe490.chunk.js"
+  },
+  {
+    "revision": "0fd6d5f6673700a5375f",
+    "url": "/abs-with-kevin-2/static/css/main.668a1a75.chunk.css"
+  },
+  {
+    "revision": "ec49592d6712cb6b5e06",
+    "url": "/abs-with-kevin-2/static/css/2.cb67da6f.chunk.css"
+  },
+  {
+    "revision": "9c5707f0b7d6264a0350e4bcd68e4425",
+    "url": "/abs-with-kevin-2/index.html"
+  }
+];
