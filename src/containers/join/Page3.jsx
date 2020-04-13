ab4445345a0bbc8ef848c2634cde91583a24f9f5
kevinjyee <kevinyee@utexas.edu>
2020-04-13 06:40:00 -0400

    sorta responsive

--- src/containers/join/Page3.jsx
+++ src/containers/join/Page3.jsx
@@ -14,13 +14,23 @@ function Page3() {
                 <h2>Testimonials</h2>
                 <OverPack>
                     <QueueAnim key="queue" type="bottom" leaveReverse className="page2-content">
-                            <p key="p" className="page-content">
-                                I hope I can meet my Q1 OKRs after this. -Kevin Yee
-                            </p>
+                        <p key="p" className="page-content">
+                            Better workout than Barry's. -Aaron Lu
+                        </p>
+
+                        <p key="p" className="page-content">
+                            Cara came in to the office to check on me because she thought I was having a heart attack.
+                            Those are just sounds of fat burning!! -Winston Brasor
+                        </p>
+
                         <p key="p" className="page-content">
                             Perfect way to get off your my computer for couple minutes,
                             connect with others and get a little workout
                             in this crazy sit at home times. -Daniel Guettler
+                        </p>
+
+                        <p key="p" className="page-content">
+                            I hope I can meet my Q1 OKRs after this. -Kevin Yee
                         </p>
 
                     </QueueAnim>
